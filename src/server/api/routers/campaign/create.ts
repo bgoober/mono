6dd3a89b9f8170cb7e69062f16b42118199038b0
Memewtoo <myuutuh@gmail.com>
2024-10-04 01:17:26 +0800

    Removed backers from campaign router

--- src/server/api/routers/campaign/create.ts
+++ src/server/api/routers/campaign/create.ts
@@ -26,31 +26,3 @@ export const createCampaign = protectedProcedure
 
     return campaign;
   });
-
-export const createBacker = protectedProcedure
-  .input(z.object({
-    amount: z.number().int().positive(),
-    message: z.string(),
-    campaignId: z.string(),
-  }))
-  .mutation(async ({ ctx, input }) => {
-    const { amount, message, campaignId } = input;
-    const { user } = ctx.session;
-
-    // Create Backer
-    const backer = await ctx.db.backer.create({
-      data: {
-        amount,
-        message,
-        campaign: { connect: { id: campaignId } },
-        user: { connect: { id: user.id } },
-      },
-    });
-
-    await ctx.db.campaign.update({
-      where: { id: campaignId },
-      data: { current: { increment: amount } },
-    });
-
-    return backer;
-  });
--- src/server/api/routers/campaign/index.ts
+++ src/server/api/routers/campaign/index.ts
@@ -1,14 +1,12 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { createCampaign, createBacker } from "./create";
+import { createCampaign } from "./create";
 import { getCampaigns, getCampaign, searchCampaigns } from "./read";
-import { updateCampaign, updateBacker } from "./update";
+import { updateCampaign } from "./update";
 
 export const campaignRouter = createTRPCRouter({
   create: createCampaign,
-  createBacker: createBacker,
   getAll: getCampaigns,
   getOne: getCampaign,
   search: searchCampaigns,
   update: updateCampaign,
-  updateBacker: updateBacker,
 });
